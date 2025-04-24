53c9d0ca544a4142895663b097aeaf63e475bef4
laozefei <3302816060@qq.com>
2025-04-24 20:07:50 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LayoutView from "@/views/LayoutView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: LayoutView,
+      children: [
+        {
+          path: "",
+          component: () => import("@/views/HomeView.vue"),
+        },
+        {
+          path: "category",
+          component: () => import("@/views/CategoryView.vue"),
+        },
+      ],
+    },
+    {
+      path: "/login",
+      component: () => import("@/views/LoginView.vue"),
+    },
+  ],
+});
+
+export default router;
