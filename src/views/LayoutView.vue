fe5fb20eed7965bce5f9b0caae42e3e795b8c1c8
laozefei <3302816060@qq.com>
2025-05-06 15:26:21 +0800

    feat:加载的时候获取导航栏列表

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -1,12 +1,21 @@
 <template>
+  <!-- 顶栏导航组件 -->
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
   <LayoutFooter />
 </template>
-
 <script setup>
 import LayoutNav from "@/components/layout/LayoutNav.vue";
 import LayoutHeader from "@/components/layout/LayoutHeader.vue";
 import LayoutFooter from "@/components/layout/LayoutFooter.vue";
+import LayoutFixed from "@/components/layout/LayoutFixed.vue";
+import { useCategoryStore } from "@/stores/category";
+import { onMounted } from "vue";
+
+const categoryStore = useCategoryStore();
+onMounted(() => {
+  categoryStore.getCategory();
+});
 </script>
