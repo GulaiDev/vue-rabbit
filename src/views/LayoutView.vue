ad8bb3d9622f6864a7643ae09ab9c9a82fe4a8a4
laozefei <3302816060@qq.com>
2025-05-06 14:45:45 +0800

    feat:添加LayoutNav,LayoutHeader,LayoutFooter组件

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    我是首页
-    <!--二级路由出口组件-->
-    <RouterView />
-  </div>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
+
+<script setup>
+import LayoutNav from "@/components/layout/LayoutNav.vue";
+import LayoutHeader from "@/components/layout/LayoutHeader.vue";
+import LayoutFooter from "@/components/layout/LayoutFooter.vue";
+</script>
