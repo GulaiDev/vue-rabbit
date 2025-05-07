ebfca361a1749750cb30423c6b3f4c870c5acf9c
laozefei <3302816060@qq.com>
2025-05-07 09:55:24 +0800

    feat:引进布局组件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,17 @@
+<script setup>
+import HomeCategory from "@/components/home/HomeCategory.vue";
+import HomeBanner from "@/components/home/HomeBanner.vue";
+import HomeNew from "@/components/home/HomeNew.vue";
+import HomeHot from "@/components/home/HomeHot.vue";
+import homeProduct from "@/components/home/HomeProduct.vue";
+</script>
+
 <template>
-  <div>HOME</div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
