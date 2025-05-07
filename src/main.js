5668a57ae828ee3034767f893917d3572a227f2d
laozefei <3302816060@qq.com>
2025-05-07 09:49:51 +0800

    feat:引入懒加载指令

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import "./styles/common.scss";
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+// 全局指令注册
+import { lazyPlugin } from "@/directives";
 
 import App from "./App.vue";
 import router from "./router";
@@ -9,5 +12,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(lazyPlugin);
 app.mount("#app");
